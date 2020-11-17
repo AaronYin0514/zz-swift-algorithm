79e40d11198e067030307f7c88506af830f7879f
Aaron <yinzhongzheng0514@zto.com>
2020-11-17 13:26:13 +0800

    临时提交

--- Queue/QueueTests/QueueTests.swift
+++ Queue/QueueTests/QueueTests.swift
@@ -1,14 +1,14 @@
 //
-//  LinkedListTests.swift
-//  LinkedListTests
+//  QueueTests.swift
+//  QueueTests
 //
-//  Created by AaronYin on 2020/11/14.
+//  Created by Aaron on 2020/11/17.
 //
 
 import XCTest
-@testable import LinkedList
+@testable import Queue
 
-class LinkedListTests: XCTestCase {
+class QueueTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,6 +21,13 @@ class LinkedListTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
     }
 
 }
---
+++ Queue/README.md
@@ -0,0 +1,16 @@
+## 队列（queue）
+
+### 队列
+
+> **队列**（queue）是一种先进先出（first in first out，缩写**FIFO**）的线性表。
+>
+> 它只允许在表的一端进行插入，而在另一端删除元素。
+
+![queue](https://raw.githubusercontent.com/AaronYin0514/zz-swift-algorithm/main/Queue/queue.png)
+
+### 双端队列(deque)
+
+> **双端队列**是限定插入和删除操作在表的两端进行的线性表。
+
+![deque](https://raw.githubusercontent.com/AaronYin0514/zz-swift-algorithm/main/Queue/deque.png)
+
