e70e8edcc160c57a359ab8c8cb3ee69c5caa1a65
aaron <562540603@qq.com>
2020-11-28 20:27:57 +0800

    临时

--- BinarySearchTree/BinarySearchTree/BinarySearchTree.swift
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.swift
@@ -24,6 +24,57 @@ public enum BinarySearchTreeNode<T: Comparable> {
         }
     }
     
+    public var isEmpty: Bool {
+        if case .empty = self {
+            return true
+        }
+        return false
+    }
+    
+    public var height: Int {
+        switch self {
+        case .empty:
+            return 0
+        case .leaf(_):
+            return 1
+        case let .node(left, _, right):
+            return 1 + max(left.height, right.height)
+        }
+    }
+    
+    public mutating func append(_ newValue: T) {
+        self = _append(newValue)
+    }
+    
+    public mutating func removeAll() {
+        self = .empty
+    }
+    
+    // MARK: - Private
+    
+    private func _append(_ newValue: T) -> BinarySearchTreeNode {
+        switch self {
+        case .empty:
+            return .leaf(newValue)
+        case .leaf(let value):
+            if newValue < value {
+                return .node(.leaf(newValue), value, .empty)
+            } else if newValue > value {
+                return .node(.empty, value, .leaf(newValue))
+            } else {
+                return .leaf(newValue)
+            }
+        case .node(let left, let value, let right):
+            if newValue < value {
+                return .node(left._append(newValue), value, right)
+            } else if newValue > value {
+                return .node(left, value, right._append(newValue))
+            } else {
+                return .node(left, newValue, right)
+            }
+        }
+    }
+    
     
     
 }
