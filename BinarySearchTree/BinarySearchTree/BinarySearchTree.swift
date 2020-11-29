643a9b304af614da666aa247bb860fd8092a1664
aaron <562540603@qq.com>
2020-11-29 12:57:41 +0800

    linshi

--- BinarySearchTree/BinarySearchTree/BinarySearchTree.swift
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.swift
@@ -7,74 +7,227 @@
 
 import Foundation
 
-public enum BinarySearchTreeNode<T: Comparable> {
+// MARK: - BinarySearchTree
+
+public struct BinarySearchTree<Element: Comparable> {
     
-    case empty
-    case leaf(T)
-    indirect case node(BinarySearchTreeNode, T, BinarySearchTreeNode)
+    fileprivate typealias Node = BinarySearchTreeNode<Element>
+    
+    private var root: Node?
+    
+    public init(value: Element? = nil) {
+        if value != value {
+            root = .leaf(value!)
+        }
+    }
     
     public var count: Int {
-        switch self {
-        case .empty:
-            return 0
-        case .leaf(_):
-            return 1
-        case let .node(left, _, right):
-            return left.count + 1 + right.count
-        }
+        root?.count ?? 0
     }
     
     public var isEmpty: Bool {
-        if case .empty = self {
-            return true
-        }
-        return false
+        root != nil
     }
     
     public var height: Int {
-        switch self {
-        case .empty:
-            return 0
-        case .leaf(_):
-            return 1
-        case let .node(left, _, right):
-            return 1 + max(left.height, right.height)
-        }
+        root?.height ?? 0
     }
     
-    public mutating func append(_ newValue: T) {
-        self = _append(newValue)
+    public mutating func append(_ newValue: Element) {
+        root = root?.append(newValue) ?? .leaf(newValue)
     }
     
     public mutating func removeAll() {
-        self = .empty
+        root = nil
     }
     
-    // MARK: - Private
+    public func contains(value: Element) -> Bool {
+        root?.search(value) != nil
+    }
     
-    private func _append(_ newValue: T) -> BinarySearchTreeNode {
-        switch self {
-        case .empty:
-            return .leaf(newValue)
-        case .leaf(let value):
-            if newValue < value {
-                return .node(.leaf(newValue), value, .empty)
-            } else if newValue > value {
-                return .node(.empty, value, .leaf(newValue))
-            } else {
-                return .leaf(newValue)
-            }
-        case .node(let left, let value, let right):
-            if newValue < value {
-                return .node(left._append(newValue), value, right)
-            } else if newValue > value {
-                return .node(left, value, right._append(newValue))
-            } else {
-                return .node(left, newValue, right)
+    // MARK: - Traverse
+    
+    public func traversePreOrder(process: (Element) -> Void) {
+        root?.traversePreOrder(process)
+    }
+    
+    public func traverseInOrder(process: (Element) -> Void) {
+        root?.traverseInOrder(process)
+    }
+    
+    public func traversePostOrder(process: (Element) -> Void) {
+        root?.traversePostOrder(process)
+    }
+    
+    public func traverseLevelOrder(process: (Element) -> Void) {
+        if root == nil { return }
+        let queue = Queue<Node>()
+        queue.enqueue(root!)
+        while !queue.isEmpty {
+            let node = queue.dequeue()
+            switch node {
+            case let .leaf(value):
+                process(value)
+            case let .node(left, value, right):
+                process(value)
+                if left != nil { queue.enqueue(left!) }
+                if right != nil { queue.enqueue(right!) }
+            default:
+                continue
             }
         }
     }
     
+    // MARK: - Remove
     
+    public func remove(_ value: Element) -> BinarySearchTree<Element> {
+        var tree = BinarySearchTree<Element>()
+        traverseLevelOrder {
+            if $0 != value {
+                tree.append($0)
+            }
+        }
+        return tree
+    }
+    
+}
+
+// MARK: - Extension Map
+
+extension BinarySearchTree {
+    
+    public func map<T>(formula: (Element) -> T) -> BinarySearchTree<T> {
+        var tree = BinarySearchTree<T>()
+        traverseLevelOrder {
+            tree.append(formula($0))
+        }
+        return tree
+    }
+    
+    public func flatMap<T>(formula: (Element) -> BinarySearchTree<T>) -> BinarySearchTree<T> {
+        var tree = BinarySearchTree<T>()
+        traverseLevelOrder {
+            formula($0).traverseLevelOrder {
+                tree.append($0)
+            }
+        }
+        return tree
+    }
+    
+}
+
+// MARK: - Node
+
+enum BinarySearchTreeNode<T: Comparable> {
+    
+    case leaf(T)
+    indirect case node(BinarySearchTreeNode?, T, BinarySearchTreeNode?)
+    
+    var count: Int {
+        switch self {
+        case .leaf(_):
+            return 1
+        case let .node(left, _, right):
+            return (left?.count ?? 0) + 1 + (right?.count ?? 0)
+        }
+    }
+    
+    var height: Int {
+        switch self {
+        case .leaf(_):
+            return 1
+        case let .node(left, _, right):
+            return 1 + max(left?.height ?? 0, right?.height ?? 0)
+        }
+    }
+    
+    var maxNode: BinarySearchTreeNode<T> {
+        switch self {
+        case let .node(_, _, right?):
+            return right.maxNode
+        default:
+            return self
+        }
+    }
+    
+    var minNode: BinarySearchTreeNode<T> {
+        switch self {
+        case let .node(left?, _, _):
+            return left.maxNode
+        default:
+            return self
+        }
+    }
+    
+    func append(_ newValue: T) -> BinarySearchTreeNode {
+        switch self {
+        case .leaf(let value):
+            if newValue < value {
+                return .node(.leaf(newValue), value, nil)
+            } else if newValue > value {
+                return .node(nil, value, .leaf(newValue))
+            } else {
+                return .leaf(newValue)
+            }
+        case let .node(left, value, right):
+            if value == newValue {
+                return .node(left, newValue, right)
+            } else if newValue < value {
+                let newLeft = left?.append(newValue) ?? .leaf(newValue)
+                return .node(newLeft, newValue, right)
+            } else {
+                let newRight = right?.append(newValue) ?? .leaf(newValue)
+                return .node(left, newValue, newRight)
+            }
+        }
+    }
+    
+    func search(_ value: T) -> BinarySearchTreeNode<T>? {
+        switch self {
+        case let .leaf(_value) where _value == value:
+            return self
+        case let .node(_, _value, _) where _value == value:
+            return self
+        case let .node(left?, _value, _) where _value > value:
+            return left.search(value)
+        case let .node(_, _value, right?) where _value < value:
+            return right.search(value)
+        default:
+            return nil
+        }
+    }
+    
+    func traversePreOrder(_ process: (T) -> Void) {
+        switch self {
+        case let .leaf(value):
+            process(value)
+        case let .node(left, value, right):
+            process(value)
+            left?.traversePreOrder(process)
+            right?.traversePreOrder(process)
+        }
+    }
+    
+    func traverseInOrder(_ process: (T) -> Void) {
+        switch self {
+        case let .leaf(value):
+            process(value)
+        case let .node(left, value, right):
+            left?.traverseInOrder(process)
+            process(value)
+            right?.traverseInOrder(process)
+        }
+    }
+    
+    func traversePostOrder(_ process: (T) -> Void) {
+        switch self {
+        case let .leaf(value):
+            process(value)
+        case let .node(left, value, right):
+            left?.traversePostOrder(process)
+            right?.traversePostOrder(process)
+            process(value)
+        }
+    }
     
 }
--- BinarySearchTree/BinarySearchTree/ViewController.swift
+++ BinarySearchTree/BinarySearchTree/ViewController.swift
@@ -12,6 +12,53 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var tree = BinarySearchTree(value: 7)
+        tree.append(4)
+        tree.append(9)
+        tree.append(2)
+        tree.append(5)
+        tree.append(8)
+        tree.append(11)
+        tree.append(1)
+        tree.append(3)
+        tree.append(10)
+        tree.append(12)
+        
+        print("-------- 前序遍历 --------")
+        tree.traversePreOrder {
+            print($0)
+        }
+        print("-------- 中序遍历 --------")
+        tree.traverseInOrder {
+            print($0)
+        }
+        
+        print("-------- 后序遍历 --------")
+        tree.traversePostOrder {
+            print($0)
+        }
+        
+        print("-------- 层序遍历 --------")
+        tree.traverseLevelOrder {
+            print($0)
+        }
+        
+        let newTree = tree.map {
+            $0 * 2
+        }
+        
+        print("-------- 层序遍历(newTree) --------")
+        newTree.traverseLevelOrder {
+            print($0)
+        }
+        
+        print("-------- 删除元素 --------")
+        let removeTree1 = tree.remove(4)
+        removeTree1.traverseLevelOrder {
+            print($0)
+        }
+        
     }
 
 
